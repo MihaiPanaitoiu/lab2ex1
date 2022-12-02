651cc55e7d9f1d06a51d217f3b084eef22fd21f4
mihai.panaitoiu <mihai.panaitoiu@softem.ro>
2022-12-02 14:35:43 +0200

    Add project files.

---
+++ lab2ex1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lab2ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Ex 1: Scrieti un program care va afisa suma cifrelor unui numar n, n fiind
+                    citit de la tastatura.
+             */
+
+            Console.WriteLine("Numarul n este: ");
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            while (n > 0)
+            {
+                sum = sum + n % 10;
+                n = n / 10;
+            }
+
+            Console.WriteLine("Suma cifrelor numarului este: " + sum);
+        }
+    }
+}
